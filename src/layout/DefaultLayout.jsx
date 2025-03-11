a7be802386b6379fd5d0b7ae827b08d6cb3b4351
Mirko <mirkocaccavone03@gmail.com>
2025-03-11 15:41:21 +0100

    [feat] add Header component and DefaultLayout for main structure

---
+++ src/layout/DefaultLayout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+
+import Header from "../components/Header";
+
+const DefaultLayout = () => {
+    return (
+        <>
+            <Header />
+            <main className="container">
+                <Outlet />
+            </main>
+        </>
+
+    )
+}
+
+export default DefaultLayout
