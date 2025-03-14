69965374b1aaf91d9d799bc119d87a9272527a46
Mirko <mirkocaccavone03@gmail.com>
2025-03-14 16:54:04 +0100

    [feat] add Loader component for displaying loading state

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,13 +1,29 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import Header from "../components/Header";
+import Loader from "../components/Loader";
 
 const DefaultLayout = () => {
+
+    const [loading, setLoading] = useState(true);
+    const location = useLocation()
+
+    // Cambia lo stato del loader ogni volta che cambia la pagina
+    useEffect(() => {
+        // Mostra il loader all'inizio del caricamento
+        setLoading(true);
+        const timer = setTimeout(() => {
+            // Nascondi il loader quando la pagina è completamente caricata
+            setLoading(false);
+        });
+    }, [location]);
+
     return (
         <>
             <Header />
             <main>
-                <Outlet />
+                {loading ? <Loader /> : <Outlet />}
             </main>
         </>
 
